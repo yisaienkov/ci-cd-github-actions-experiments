7e17c2f5aded22cde3eaab29c2d6d2ffc36b898a
Yaroslav Isaienkov <oiuygl@gmail.com>
2020-10-25 22:28:50 +0200

    Fix

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -4,16 +4,10 @@ from src import calc
 
 
 def test_1():
-    """
-    Some test.
-
-    """
+    """Some test."""
     assert calc.new_add(2, 3) == 5
 
 
 def test_2():
-    """
-    Some test.
-
-    """
+    """Some test."""
     assert calc.new_pow(2, 3) == 8
